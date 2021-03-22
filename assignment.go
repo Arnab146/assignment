dbf153cc5a6ce8cb5adbb5ff51becf35f56c3a9c
Arnab146 <arnabsaha146@gmail.com>
2021-03-22 12:36:38 +0530

    rebase1

--- assignment.go
+++ assignment.go
@@ -8,7 +8,5 @@ func main() {
 	fmt.Printf("Second code code")
 	fmt.Printf("Second code code")
 	fmt.Printf("Second code code146")
-	fmt.Printf("third code 146")
-	fmt.Printf("fourth code 146")
 
 }
